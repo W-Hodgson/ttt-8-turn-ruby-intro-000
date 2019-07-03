845fccfb26ad2ec3a8b0b6e35d67007ff9b6e2cd
Will Hodgson <will.hodgson@current-rms.com>
2019-07-03 10:36:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  move(board, index, 'X')
+end
+
 def move(board, index, token="X")
   valid = valid_move?(board, index)
   if valid
